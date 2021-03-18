fb292671ffd464312fe3205f364914c42fa5feab
Brian Whicheloe <brian@whicheloe.us>
2021-03-17 18:24:43 -0700

    Update ecmaversion to 2021

--- index.js
+++ index.js
@@ -3,7 +3,7 @@
 
 module.exports = {
     parserOptions: {
-        ecmaVersion: 9,
+        ecmaVersion: 2021,
         sourceType: 'module',
         ecmaFeatures: {
             impliedStrict: true,
