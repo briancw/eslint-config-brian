cc6843a79e70cc7ae63d9b85a534e97df3e97a1b
Brian Whicheloe <brian@whicheloe.us>
2023-06-10 11:05:49 -0700

    Remove style based rules, add resolver directly

--- index.js
+++ index.js
@@ -3,6 +3,7 @@ module.exports = {
         'eslint-config-xo',
         'plugin:unicorn/recommended',
         'plugin:import/recommended',
+        'plugin:import/typescript',
     ],
     plugins: [
         'unicorn',
@@ -10,47 +11,31 @@ module.exports = {
         'no-autofix',
     ],
     rules: {
-        'indent': ['error', 4, {MemberExpression: 0}],
         'camelcase': ['error'],
-        'brace-style': ['error', 'stroustrup', {
-            allowSingleLine: false,
-        }],
-        'semi': ['error', 'never'],
-        // This is actually in XO config, but I want to be extra sure
-        'comma-dangle': ['error', 'always-multiline'],
-        'quote-props': ['error', 'consistent-as-needed'],
-        'quotes': ['error', 'single', {
-            allowTemplateLiterals: true,
-        }],
-        'arrow-parens': ['error', 'always'],
-        // This rule is identical to XO's except for anonymous functions
-        'space-before-function-paren': ['error', {
-            anonymous: 'never',
-            named: 'never',
-            asyncArrow: 'always',
-        }],
-        'no-warning-comments': ['off'],
-        'capitalized-comments': ['off'],
         'func-style': ['error', 'expression'],
+        // 'no-warning-comments': ['off'],
+        // 'capitalized-comments': ['off'],
+
+        // XO overrides
+        'padding-line-between-statements': ['off'],
         
         // Unicorn Rules Overrides
-        'unicorn/no-for-loop': ['off'],
-        'unicorn/no-array-for-each': ['off'],
-        'unicorn/filename-case': ['error', {
-            cases: {camelCase: true},
-        }],
-        'padding-line-between-statements': ['off'],
-        // If "dev" is good enough for chrome devtools its good enough for me
-        'unicorn/prevent-abbreviations': ['error', {
-            allowList: {
-                dev: true,
-            },
-        }],
-        // I know when I should and shouldn't use a ternary
-        'unicorn/prefer-ternary': ['off'],
+        'unicorn/no-for-loop': 'off',
+        'unicorn/no-array-for-each': 'off',
+        'unicorn/filename-case': 'off',
+        'unicorn/prevent-abbreviations': 'off',
+        'unicorn/prefer-ternary': 'off', // I know when I should and shouldn't use a ternary
+        'unicorn/prefer-switch': 'off',
 
         // Don't autofix prefer-const
         'prefer-const': 'off',
         'no-autofix/prefer-const': 'warn',
+    },
+    // TODO this doesn't seem necessary anymore, but the docs recommend it
+    // Check up in a few months and see if it's still strictly necessary
+    settings: {
+        'import/resolver': {
+            typescript: true,
+        },
     },
 }
